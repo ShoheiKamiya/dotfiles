dce938e1936265a3cd9170a363ce0c7cd991ba50
ShoheiKamiya <shoooooo.bqt@gmail.com>
2023-05-09 23:39:43 +0900

    feat: add .gitignore

--- bin/link.sh
+++ bin/link.sh
@@ -5,3 +5,4 @@ ln -s "${REPO_PATH}"/dotfiles/nvim ~/.config/nvim
 ln -s "${REPO_PATH}"/dotfiles/.Brewfile ~
 ln -s "${REPO_PATH}"/dotfiles/.env ~
 ln -s "${REPO_PATH}"/dotfiles/.zshrc ~
+ln -s "${REPO_PATH}"/dotfiles/.gitconfig ~

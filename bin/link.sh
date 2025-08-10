7f3cdd65abd95b26b86c2adfe866eb64151c53e8
ShoheiKamiya <shoooooo.bqt@gmail.com>
2025-08-10 21:52:45 +0900

    claude codeの設定を追加

--- bin/link.sh
+++ bin/link.sh
@@ -5,9 +5,10 @@ SCRIPT_PATH=$(cd $(dirname $0) && pwd)
 ln -s -i "${SCRIPT_PATH}"/../.config/iterm2/com.googlecode.iterm2.plist ~/.config/iterm2
 ln -s -i "${SCRIPT_PATH}"/../.config/nvim ~/.config
 ln -s -i "${SCRIPT_PATH}"/../.config/git ~/.config
-
 ln -s -i "${SCRIPT_PATH}"/../.Brewfile ~
 ln -s -i "${SCRIPT_PATH}"/../Gemfile ~
 ln -s -i "${SCRIPT_PATH}"/../.env ~
 ln -s -i "${SCRIPT_PATH}"/../.snippets ~
 ln -s -i "${SCRIPT_PATH}"/../.zshrc ~
+ln -s -i "${SCRIPT_PATH}"/../.claude/settings.json ~/.claude/
+ln -s -i "${SCRIPT_PATH}"/../.claude/CLAUDE.md ~/.claude/
